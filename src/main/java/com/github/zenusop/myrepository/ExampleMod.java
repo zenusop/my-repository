9d3b2ee84135ef0ec8b346f14c427208807d499a
GitHub Action <action@github.com>
2025-01-30 12:39:15 +0000

    Initialize template repository proper
    Set package name to com.github.zenusop.myrepository
    Set project name to my-repository
    Set modid to myrepository

--- src/main/java/com/github/zenusop/myrepository/ExampleMod.java
+++ src/main/java/com/github/zenusop/myrepository/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.zenusop.myrepository;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "myrepository", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
